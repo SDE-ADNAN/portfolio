b3d9cd697dbf8f5d513f9202ce9b5ac81fd6ef4e
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-08-15 00:41:50 +0530

    signing off with 1:09:38 playtime

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,18 +1,18 @@
-import './App.scss';
+import "./App.scss";
 
-import { About , Footer , Header , Skills , Testimonial , Work } from "./containers";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./containers";
 import { Navbar } from "./components";
 
 function App() {
   return (
     <div className="app">
-      <Navbar/>
-      <Header/>
-      <About/>
-      <Work/>
-      <Skills/>
-      <Testimonial/>
-      <Footer/>
+      {/* <Navbar/> */}
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
--- front_end/src/containers/Header/Header.jsx
+++ front_end/src/containers/Header/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import "./Header.scss"
+import React from "react";
+import { motion } from "framer-motion";
+
+import "./Header.scss";
+import { images } from "../../constants";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 1 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cpm app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am </p>
+              <h1 className="head-text">Adnan</h1>
+            </div>
+          </div>
+          <div className="tag-cmp app__flex"></div>
+        </div>
+      </motion.div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
