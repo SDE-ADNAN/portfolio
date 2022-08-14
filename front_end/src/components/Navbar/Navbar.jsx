0409b0b9993caa228c6dfb238375933c21c3621e
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-08-14 21:48:08 +0530

    added the consize import for assets

--- front_end/src/components/Navbar/Navbar.jsx
+++ front_end/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 import "./Navbar.scss"
+import {images} from "../../constants"
 
 function Navbar() {
   return (
     <nav>
         <div>
-            <img/>
+            <img src={images.logo} alt="logo" />
         </div>
+        <ul>
+            
+        </ul>
     </nav>
   )
 }
