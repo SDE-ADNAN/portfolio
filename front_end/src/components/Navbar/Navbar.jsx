850f779101585a29a452fe03bdcf1f9bb70890ac
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-12-19 02:28:54 +0530

    som bug

--- front_end/src/components/Navbar/Navbar.jsx
+++ front_end/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
-import logo from "../../assets/Logo.png";
+import logo from "./Logo.png";
 
 import "./Navbar.scss";
 
